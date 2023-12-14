1ea5254cab77e2a1721d7738fd94c9d0d1431fed
JarredEagley <jarredeagley@gmail.com>
2023-12-14 12:39:32 -0600

    Rotor test; result is correct but floating point equality is not.

--- src/tests/test_transform.rs
+++ src/tests/test_transform.rs
@@ -7,7 +7,52 @@ mod test_reflections {
 
 #[cfg(test)]
 mod test_rotors {
+    use crate::{vectors::bivector::Bivector, extras::{angle::Angle, transformations::{Rotor, RigidTransformation}, point2d::Point2d}};
 
+    #[test]
+    fn test_rotor_bivector() {
+        // The point we're rotating around.
+        let axis = Bivector {
+            e20: 4.5f32,
+            e01: 7.2f32,
+            e12: 1.0,
+        };
+        // The point we're rotating.
+        let point = Bivector {
+            e20: 3.2f32,
+            e01: -4.2,
+            e12: 1.0,
+        };
+        // The point that should result from this rotation.
+        let correct_result = Bivector {
+            e20: 11.64177848998413f32,
+            e01: -1.780256121069154,
+            e12: 1.0,
+        };
+
+        // Build the rotor and perform the rotation.
+        let rotor_angle = Angle::from_degrees(45.0f32);
+        let rotor = Rotor::new(Point2d::from_bivector(&axis), rotor_angle);
+
+        let result = rotor.apply(&point);
+
+        assert_eq!(result, correct_result)
+    }
+    
+    #[test]
+    fn test_rotor_bivector_at_infinity() {
+        todo!()
+    }
+    
+    #[test]
+    fn test_rotor_vector() {
+        todo!()
+    }
+    
+    #[test]
+    fn test_rotor_multivector() {
+        todo!()
+    }
 }
 
 #[cfg(test)]

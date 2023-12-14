f69f048c3707bb0f13b1bc9f5fbf972bf81186c8
JarredEagley <jarredeagley@gmail.com>
2023-12-14 12:57:58 -0600

    Equality comparison will be pulled out into its own module

--- src/lib.rs
+++ src/lib.rs
@@ -2,4 +2,8 @@ pub mod vectors;
 pub mod traits;
 pub mod extras;
 
+/// Equality comparisons for k-vectors and multivectors.
+/// Included as its own module incase someone wants to change the floating point epsilons.
+pub mod equality;
+
 mod tests;

ee497b6031918dcb116ab5cfbc7b15c46435b131
JarredEagley <jarredeagley@gmail.com>
2023-12-14 15:06:26 -0600

    cleared out main for now

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,3 @@
-use simply_2dpga::{vectors::{vector::Vector, bivector::Bivector}, traits::{Contraction, RegressiveProduct}, extras::{point2d::{Point2d, self}, transformations::{Rotor}, angle::Angle, transformations, transformations::Motor}};
-
 fn main() {
-    // sandbox for testing purposes.
-
-    let point1 = Point2d::new(2.0f32, 0.0); // axis
-    let point2 = Point2d::new(2.0f32, 4.0); // point i want to rotate.
-
-    // let angle: Angle<f32> = Angle::from_radians(3.1415926535);
-    let angle: Angle<f32> = Angle::from_degrees(45.0f32);
-
-
-    // println!("result:\n\t{}", Point2d::from_bivector(&result));
+    // sandbox
 }

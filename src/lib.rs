6dccfe2149a819c95d4584e7dfd02dca47c7a7ac
JarredEagley <jarredeagley@gmail.com>
2023-12-15 15:11:01 -0600

    Simple prelude file

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,7 @@
 pub mod defs;
 pub mod traits;
 pub mod extras;
+pub mod prelude;
 
 /// Equality comparisons for k-vectors and multivectors.
 /// Included as its own module incase someone wants to change the floating point epsilons.
---
+++ src/prelude.rs
@@ -0,0 +1,4 @@
+// Users can manually include k-vectors if they want to use them.
+pub use crate::defs::{multivector::*, vector::*, bivector::*, trivector::*};
+// We want to include all geometric algebra operators.
+pub use crate::traits::*;

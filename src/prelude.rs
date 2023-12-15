e10e842966875810fb61cefad9126bd7af7a5782
JarredEagley <jarredeagley@gmail.com>
2023-12-15 15:12:27 -0600

    Added partialeq defs to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -1,4 +1,8 @@
 // Users can manually include k-vectors if they want to use them.
 pub use crate::defs::{multivector::*, vector::*, bivector::*, trivector::*};
+
 // We want to include all geometric algebra operators.
 pub use crate::traits::*;
+
+// Probably for the best that equality comparison is included by default.
+pub use crate::equality::*;
